e08282e25e5ff1d35dba7fad2280db4b517dbf6f
Felipe Gomes <felipegomesfurtado@gmail.com>
2024-10-21 10:54:51 -0300

    fix: hidratation warning on theme-switcher.tsx component

--- src/components/theme-switcher.tsx
+++ src/components/theme-switcher.tsx
@@ -1,23 +1,27 @@
 'use client'
 
-import { Moon, Sun } from 'lucide-react'
+import * as React from 'react'
+import { MoonIcon, SunIcon } from '@radix-ui/react-icons'
 import { useTheme } from 'next-themes'
+
+import { Button } from '@/components/ui/button'
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from './ui/dropdown-menu'
-import { Button } from './ui/button'
+} from '@/components/ui/dropdown-menu'
 
 export function ThemeSwitcher() {
-  const { resolvedTheme, setTheme } = useTheme()
+  const { setTheme } = useTheme()
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon">
-          {resolvedTheme === 'light' && <Sun size={16} />}
-          {resolvedTheme === 'dark' && <Moon size={16} />}
+        <Button variant="outline" size="icon">
+          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
